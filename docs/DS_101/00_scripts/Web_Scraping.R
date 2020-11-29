6441fd5e23ad4475a3d4256467be395efb3bd104
netrox93 <51364705+netrox93@users.noreply.github.com>
2020-11-29 14:49:00 +0100

    Chapter4

---
+++ docs/DS_101/00_scripts/Web_Scraping.R
@@ -0,0 +1,36 @@
+# WEBSCRAPING ----
+
+# 1.0 LIBRARIES ----
+
+library(tidyverse) # Main Package - Loads dplyr, purrr, etc.
+library(rvest)     # HTML Hacking & Web Scraping
+library(xopen)     # Quickly opening URLs
+library(jsonlite)  # converts JSON files to R objects
+library(glue)      # concatenate strings
+library(stringi)   # character string/text processing
+
+# 1.1 COLLECT PRODUCT FAMILIES ----
+
+url_home          <- "https://www.rosebikes.de/"
+xopen(url_home) # Open links directly from RStudio to inspect them
+
+# Read in the HTML for the entire webpage
+html_home         <- read_html(url_home)
+
+# Web scrape the ids for the families
+bike_family_tbl <- html_home %>%
+  
+  # Get the nodes for the families ...
+  html_nodes(css = ".js-navigationDrawer__list--secondary") %>%
+  # ...and extract the information of the id attribute
+  html_attr('id') %>%
+  
+  # Convert vector to tibble
+  enframe(name = "position", value = "family_class") %>%
+  
+  # Add a hashtag so we can get nodes of the categories by id (#)
+  mutate(
+    family_id = str_glue("#{family_class}")
+  )
+
+bike_family_tbl
